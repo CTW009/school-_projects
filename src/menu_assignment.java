d48f85d17f7abd15810daf482c3b6dcd9b0c75ef
CTW009 <connerwatson869@gmail.com>
2025-10-14 18:06:00 -0400

    this is not smthing i wanna do...

--- src/menu_assignment.java
+++ src/menu_assignment.java
@@ -109,10 +109,10 @@ public class menu_assignment {
                             return 3;
                         case "4":
                             return 4;
-                        case "5":
-                            return 5;
-                        case "6":
-                            return 6;
+                        //case "5":
+                            //return 5;
+                        //case "6":
+                            //return 6;
                         default:
                             System.out.println("please enter a valid number!");
 
